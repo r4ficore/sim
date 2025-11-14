500871fc4c5877ea8e9be5af904aa7b4dc758763
r4ficore <100816536+r4ficore@users.noreply.github.com>
2025-11-14 14:01:39 +0100

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,10 @@
+// js/config.js
+
+export const defaultConfig = {
+  worldWidth: 40,
+  worldHeight: 40,
+  initialPopulation: 40,
+  initialEnergyMin: 30,
+  initialEnergyMax: 60,
+  cellSize: 15
+};
