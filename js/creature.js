8fd7c831a3255ac08828f84a20ffb2e24ccdf983
r4ficore <100816536+r4ficore@users.noreply.github.com>
2025-11-14 17:20:26 +0100

    Update creature.js

--- js/creature.js
+++ js/creature.js
@@ -1,14 +1,16 @@
 // js/creature.js
+// Etap 0: tylko definicja klasy – implementacja pojawi się później.
 
 export class Creature {
-  constructor(id, sex, x, y, energy) {
+  constructor({ id = 0, sex = 'M', x = 0, y = 0, energy = 0 }) {
     this.id = id;
-    this.sex = sex;    // 'M' albo 'F'
+    this.sex = sex;
     this.x = x;
     this.y = y;
     this.energy = energy;
-
     this.age = 0;
     this.alive = true;
+    this.reproductionCooldown = 0;
+    this.toolLevel = 0;
   }
 }
